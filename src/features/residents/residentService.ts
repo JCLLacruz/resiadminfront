b10434dca93813baa50f1d0ced5f533e0ea2a21d
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-07-19 12:04:56 +0200

    api_url fixed

--- src/features/residents/residentService.ts
+++ src/features/residents/residentService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL: string = 'https://serverresiadmin.onrender.com/residents';
+const API_URL: string = 'http://localhost:3001/residents';
 
 const getAllResidents = async () => {
 	const token = localStorage.getItem('token');
