dd42697e7e8327daa5e32e43b8af80b721f2746f
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-07-19 13:50:05 +0200

    wapp integrated

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,18 @@ import { Provider } from 'react-redux';
 import { store } from './app/store.ts';
 import { BrowserRouter } from 'react-router-dom';
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+	  navigator.serviceWorker.register('/service-worker.js')
+		.then(registration => {
+		  console.log('Service Worker registrado con éxito:', registration);
+		})
+		.catch(error => {
+		  console.log('Error al registrar el Service Worker:', error);
+		});
+	});
+  }
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<Provider store={store}>
 		<ChakraProvider theme={theme}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'ResiAdmin',
+        short_name: 'ResiAdmin',
+        description: 'Your app description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'document' || request.destination === 'script' || request.destination === 'style' || request.destination === 'image' || request.destination === 'font',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'assets-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 días
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   }
-})
+});
