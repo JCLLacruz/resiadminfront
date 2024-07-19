eb5dd4584638d2414d8f35b7e52d74acda2fea25
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-07-19 18:00:57 +0200

    vercel fixed 16

--- public/service-worker.ts
+++ public/service-worker.ts
@@ -1,25 +1,24 @@
 /// <reference lib="webworker" />
 
 self.addEventListener('install', (event: ExtendableEvent) => {
-    event.waitUntil(
-      caches.open('static-cache-v1').then((cache: Cache) => {
-        return cache.addAll([
-          './',
-          './index.html',
-          './manifest.json',
-        //   './icon-192x192.png',
-        //   './icon-512x512.png',
-          // Agrega aquí otros recursos que quieras cachear
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event: FetchEvent) => {
-    event.respondWith(
-      caches.match(event.request).then((response: Response | undefined) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('static-cache-v1').then((cache: Cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+      //   './icon-192x192.png',
+      //   './icon-512x512.png',
+        // Agrega aquí otros recursos que quieras cachear
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event: FetchEvent) => {
+  event.respondWith(
+    caches.match(event.request).then((response: Response | undefined) => {
+      return response || fetch(event.request);
+    })
+  );
+});
