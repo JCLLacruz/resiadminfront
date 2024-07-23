21464144308ddf6ca4690e0d3e235c0530dd47aa
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-07-23 11:09:58 +0200

    theme colors changed

--- src/assets/icons/icons.tsx
+++ src/assets/icons/icons.tsx
@@ -97,4 +97,4 @@ export const closeIcon = (
 	</svg>
 );
 
-export const editIcon = <svg width="2rem" height="2rem" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title/><g id="Complete"><g id="edit"><g><path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/><polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/></g></g></g></svg>
+export const editIcon = <svg width="2rem" height="2rem" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title/><g id="Complete"><g id="edit"><g><path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke={theme.colors.brand[50]} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/><polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke={theme.colors.brand[50]} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/></g></g></g></svg>
