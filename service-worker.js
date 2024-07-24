22553141ed9edbe24d99a125de917406fdedfd8c
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-07-24 09:50:05 +0200

    service-worked error chaking implemented

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,38 @@
 "use strict";
 /// <reference lib="webworker" />
+
 self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open('static-cache-v1').then((cache) => {
-        return cache.addAll([
-            './',
-            './index.html',
-            './manifest.json',
-              './public/icon-192x192.svg',
-              './public/icon-512x512.svg',
-            // Agrega aquí otros recursos que quieras cachear
-        ]);
-    }));
+    event.waitUntil(
+        caches.open('static-cache-v1')
+            .then((cache) => {
+                const resources = [
+                    './',
+                    './index.html',
+                    './manifest.json',
+                    // './public/icon-192x192.svg',
+                    // './public/icon-512x512.svg',
+                    // Add more resources here
+                ];
+
+                return Promise.all(
+                    resources.map((resource) => {
+                        return cache.add(resource).catch((error) => {
+                            console.error(`Failed to cache ${resource}:`, error);
+                        });
+                    })
+                );
+            })
+            .catch((error) => {
+                console.error('Failed to open cache:', error);
+            })
+    );
 });
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-    }));
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
 });
