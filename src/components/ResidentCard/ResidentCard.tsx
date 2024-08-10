fcce768d3877e554232304652f5946dc66e71179
Juan Carlos iMac <juancarloslacruzlacruz@outlook.com>
2024-08-10 18:51:55 +0200

    edit forms fixed

--- src/components/ResidentCard/ResidentCard.tsx
+++ src/components/ResidentCard/ResidentCard.tsx
@@ -231,12 +231,9 @@ const ResidentCard: FC = () => {
 					{resident.moreinfo}
 				</Text>
 			</Container>
-			<Modal isOpen={isOpen} onClose={onClose} size={'xl'}>
+			<Modal isOpen={isOpen} onClose={onClose} size={'lg'} isCentered={false}>
 				<ModalOverlay />
-				<ModalContent>
-					<Text position={'absolute'} top={2} right={2} fontSize={'2xl'} color={'brand.500'} cursor={'pointer'} onClick={onClose}>
-						{closeIcon}
-					</Text>
+				<ModalContent backgroundColor={'transparent'} border={'none'} boxShadow={'none'}>
 					<ModalBody>
 						{modalContent === 'images' && <AllImages images={'resident'} />}
 						{modalContent === 'form' && <ResidentForm residentProp={resident} />}
